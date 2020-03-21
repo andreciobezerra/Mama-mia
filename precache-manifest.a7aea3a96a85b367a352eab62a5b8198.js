53dae2596ccb1e53b1b6c662f24b704f8cecdef6
Andrecio Bezerra <andreciobezerra@gmail.com>
2020-03-21 17:13:40 -0300

    Updates

---
+++ precache-manifest.a7aea3a96a85b367a352eab62a5b8198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05de555d8874ac07d6a1abecc33ec47",
+    "url": "/mama-mia/index.html"
+  },
+  {
+    "revision": "23673098e59914ab33c8",
+    "url": "/mama-mia/static/css/main.8723be18.chunk.css"
+  },
+  {
+    "revision": "620bb2d9acd815fc2ee7",
+    "url": "/mama-mia/static/js/2.e6ab21a5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/mama-mia/static/js/2.e6ab21a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23673098e59914ab33c8",
+    "url": "/mama-mia/static/js/main.2444d0d1.chunk.js"
+  },
+  {
+    "revision": "58a4cd56586d4ced3100",
+    "url": "/mama-mia/static/js/runtime-main.69d17cf2.js"
+  }
+]);
